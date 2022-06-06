8b4af0ccf4ac9965a3c8f245ba6ba316f3f05bcb
Psychokiller 5 <102171274+Psychokiller-5@users.noreply.github.com>
2022-06-06 18:36:47 +0530

    Update hulk.py

--- hulk.py
+++ hulk.py
@@ -1,6 +1,6 @@
 # ----------------------------------------------------------------------------------------------
 # HULK - HTTP Unbearable Load King
-#
+# Owner Of Tool Psycho Killer 
 # this tool is a dos tool that is meant to put heavy load on HTTP servers in order to bring them
 # to their knees by exhausting the resource pool, its is meant for research purposes only
 # and any malicious usage of this tool is prohibited.
